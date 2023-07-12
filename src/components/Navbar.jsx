a1748ee207fda9e20ddf643d3e88395bee1f7edf
kunalkatara12 <kunalkatara888@gmail.com>
2023-07-12 11:34:35 +0530

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="w-full h-[15%] border">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-[500] py-2 px-4 rounded-[2rem]">
+    <div className="w-full h-[15%] border flex justify-end items-center p-2">
+      <button className="bg-blue-500 hover:bg-blue-700 h-fit text-white font-[500] py-2 px-4 rounded-[2rem]">
         + Add new task
       </button>
+      
     </div>
   );
 }
