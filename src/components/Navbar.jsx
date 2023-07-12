da65508ceefb8fe7f81564730f91f262b0a02624
kunalkatara12 <kunalkatara888@gmail.com>
2023-07-12 08:26:57 +0530

    add button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='w-full h-[15%] border'>Navbar</div>
-  )
+    <div className="w-full h-[15%] border">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-[500] py-2 px-4 rounded-[2rem]">
+        + Add new task
+      </button>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
