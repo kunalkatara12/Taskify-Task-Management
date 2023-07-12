139c09c3704296855c3c9577d7af2848c6e3b9a4
kunalkatara12 <kunalkatara888@gmail.com>
2023-07-12 12:10:29 +0530

    sidebar.jsx design

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function Sidebar() {
   return (
-    <div className='h-screen w-1/5 border'>Taskify</div>
-  )
+    <div className="box-border h-screen  w-1/5 border">
+      <header className="mb-2 p-2 text-center">Taskify</header>
+      <div className="ml-0">
+        <p className="ml-3 text-xs uppercase">All Projects</p>
+        <div className="mt-3 text-sm">
+          <div className="bg-blue-500 hover:bg-blue-700 h-[45px] max-w-[90%] no-scrollbar flex items-center justify-center p- overflow-x-scroll  border rounded-r-[50px] mb-1">
+            Platform LAunch
+          </div>
+          <div className="h-[45px] max-w-[90%] no-scrollbar flex items-center justify-center p- overflow-x-scroll  border rounded-r-[50px] mb-1">
+            MArketing Plan
+          </div>
+          <div className="h-[45px] max-w-[90%] no-scrollbar flex items-center justify-center p- overflow-x-scroll  border rounded-r-[50px] mb-1">
+            Roadmap
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
