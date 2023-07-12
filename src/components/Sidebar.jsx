ce636ec4785bb1481a4d2656ede70776c15d30d3
kunalkatara12 <kunalkatara888@gmail.com>
2023-07-12 18:46:14 +0530

    Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-
+import { GrProjects } from "react-icons/gr";
 function Sidebar() {
   return (
-    <div className="box-border h-screen  w-1/5 border">
+    <div className="box-border h-scrren  w-1/5 border">
       <header className="mb-2 p-2 text-center">Taskify</header>
       <div className="ml-0">
         <p className="ml-3 text-xs uppercase">All Projects</p>
         <div className="mt-3 text-sm">
-          <div className="bg-blue-500 hover:bg-blue-700 h-[45px] max-w-[90%] no-scrollbar flex items-center justify-center p- overflow-x-scroll  border rounded-r-[50px] mb-1">
+          <div className="bg-blue-500 hover:bg-blue-700 h-[50px] max-w-[90%] no-scrollbar flex items-center justify-start pl-7 overflow-x-scroll  border rounded-r-[50px] mb-1">
+            <GrProjects className="mr-2" />
             Platform LAunch
           </div>
-          <div className="h-[45px] max-w-[90%] no-scrollbar flex items-center justify-center p- overflow-x-scroll  border rounded-r-[50px] mb-1">
+          <div className="h-[50px] max-w-[90%] no-scrollbar flex items-center justify-start pl-7 overflow-x-scroll  border rounded-r-[50px] mb-1">
+            <GrProjects className="mr-2" />
             MArketing Plan
           </div>
-          <div className="h-[45px] max-w-[90%] no-scrollbar flex items-center justify-center p- overflow-x-scroll  border rounded-r-[50px] mb-1">
+          <div className="h-[50px] max-w-[90%] no-scrollbar flex items-center justify-start pl-7 overflow-x-scroll  border rounded-r-[50px] mb-1">
+            <GrProjects className="mr-2" />
             Roadmap
           </div>
         </div>
